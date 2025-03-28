e9475d28af56fb11266f13a166062e223efe58ed
D327x <dens241235@gmail.com>
2025-03-28 14:08:36 +0100

    Projet fonctionnel : attention démarrage robot avant caméra

--- software/raspberry/superviseur-robot/tasks.cpp
+++ software/raspberry/superviseur-robot/tasks.cpp
@@ -425,35 +425,6 @@ void Tasks::CameraTask(void *arg)
                 int LOCALRechercheRobot = RechercheRobot; // vrai lorsqu'on demande la recherche du robot
                 rt_mutex_release(&mutex_RechercheRobot);
 
-                
-                /*                              A VÉRIFIER                  */
-                if (LOCALRechercheRobot == 1)
-                {
-                    cout << "      > RECHERCHE robot en cours" << endl << flush;
-                    list<Position> liste_position = img->SearchRobot(arene);
-                    Position positionRobot;
-                    if (liste_position.empty() == false)
-                    {
-                        positionRobot = liste_position.front();
-                        img->DrawRobot(positionRobot);
-                    }
-                    else
-                    {
-                        positionRobot.center = cv::Point2f(-1.0,-1.0); // Position erreur
-                        img->DrawRobot(positionRobot);
-                    }
-                                        
-                    //position->ToString()  <= ERROR
-                    
-                    //cout << "Envoi de la position : " << positionRobot->ToString() << endl << flush;
-                   
-                    msgSend_POSITION = new MessagePosition(MESSAGE_CAM_POSITION, positionRobot);
-                    rt_mutex_acquire(&mutex_monitor, TM_INFINITE);
-                    WriteInQueue(&q_messageToMon, msgSend_POSITION);
-                    rt_mutex_release(&mutex_monitor);
-                }
-
-
                 if (LOCALdemandedetectionarene == 1)
                 {
                     
@@ -498,6 +469,10 @@ void Tasks::CameraTask(void *arg)
                     rt_mutex_acquire(&mutex_ConfirmationArene, TM_INFINITE);
                     ConfirmationArene = -1;
                     rt_mutex_release(&mutex_ConfirmationArene);
+                    rt_mutex_acquire(&mutex_attenteConfirmationArene, TM_INFINITE);
+                    AttenteConfirmationArene = 0;
+                    rt_mutex_release(&mutex_attenteConfirmationArene);
+                    cout << "        > Arène CONFIRMEE PAR L'UTILISATEUR !" << endl << flush;
                 }else if (LOCALConfirmationArene == 0){
                     rt_mutex_acquire(&mutex_detectionArene, TM_INFINITE);
                     DetectionArene = 0;
@@ -505,8 +480,45 @@ void Tasks::CameraTask(void *arg)
                     rt_mutex_acquire(&mutex_ConfirmationArene, TM_INFINITE);
                     ConfirmationArene = -1;
                     rt_mutex_release(&mutex_ConfirmationArene);
+                    rt_mutex_acquire(&mutex_attenteConfirmationArene, TM_INFINITE);
+                    AttenteConfirmationArene = 0;
+                    rt_mutex_release(&mutex_attenteConfirmationArene);
+                    cout << "        > Arène NON CONFIRMEE PAR L'UTILISATEUR !" << endl << flush;
                 }
                 else if (LOCALAttenteConfirmationArene == 0){
+                    
+                    /*                              A VÉRIFIER                  */
+                    if (LOCALRechercheRobot == 1)
+                    {
+                        cout << "      > RECHERCHE robot en cours" << endl << flush;
+                        list<Position> liste_position = img->SearchRobot(arene);
+                        Position positionRobot;
+                        if (liste_position.empty() == false)
+                        {
+                            positionRobot = liste_position.front();
+                            img->DrawRobot(positionRobot);
+                        }
+                        else
+                        {
+                            positionRobot.center = cv::Point2f(-1.0,-1.0); // Position erreur
+                            img->DrawRobot(positionRobot);
+                        }
+
+                        //position->ToString()  <= ERROR
+
+                        //cout << "Envoi de la position : " << positionRobot->ToString() << endl << flush;
+
+                        msgSend_POSITION = new MessagePosition(MESSAGE_CAM_POSITION, positionRobot);
+                        rt_mutex_acquire(&mutex_monitor, TM_INFINITE);
+                        WriteInQueue(&q_messageToMon, msgSend_POSITION);
+                        rt_mutex_release(&mutex_monitor);
+                    }
+                    
+                    if (arene.IsEmpty() == false)
+                    {
+                        img->DrawArena(arene);
+                    }
+                    
                     msgSend_IMG = new MessageImg(MESSAGE_CAM_IMAGE, img);
                     rt_mutex_acquire(&mutex_monitor, TM_INFINITE);
                     WriteInQueue(&q_messageToMon, msgSend_IMG);
@@ -517,8 +529,7 @@ void Tasks::CameraTask(void *arg)
             }
         }
     }
-    cout << endl
-         << flush;
+    cout << endl << flush;
 }
 
 #else
@@ -716,6 +727,7 @@ void Tasks::ReceiveFromMonTask(void *arg)
         else if (msgRcv->CompareID(MESSAGE_ROBOT_GO_FORWARD) ||
                  msgRcv->CompareID(MESSAGE_ROBOT_GO_BACKWARD) ||
                  msgRcv->CompareID(MESSAGE_ROBOT_GO_RIGHT) ||
+                 msgRcv->CompareID(MESSAGE_ROBOT_GO_LEFT) ||
                  msgRcv->CompareID(MESSAGE_ROBOT_STOP))
         {
 
@@ -772,9 +784,9 @@ void Tasks::ReceiveFromMonTask(void *arg)
         #endif
         else if (msgRcv->CompareID(MESSAGE_CAM_ASK_ARENA))
         {
-            rt_mutex_acquire(&mutex_detectionArene, TM_INFINITE);
-            DetectionArene = 1;
-            rt_mutex_release(&mutex_detectionArene);
+            rt_mutex_acquire(&mutex_demandeRechercheArene, TM_INFINITE);
+            DemandeRechercheArene = 1;
+            rt_mutex_release(&mutex_demandeRechercheArene);
         }
         else if (msgRcv->CompareID(MESSAGE_CAM_ARENA_CONFIRM))
         {
